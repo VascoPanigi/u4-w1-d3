50483f08518dae0b8730a2b943bd6e55a1872ac4
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-05-22 15:43:51 +0200

    es1 - completed

---
+++ src/es1/Main.java
@@ -0,0 +1,23 @@
+package es1;
+
+import es1.entities.Rectangle;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Rectangle rectangle1 = new Rectangle(25, 20);
+        Rectangle rectangle2 = new Rectangle(30, 20);
+
+        int perimeterRec1 = rectangle1.rectanglePerimeter();
+        int areaRec1 = rectangle1.rectangleArea();
+        rectangle1.stampRectangle();
+        System.out.println(perimeterRec1);
+        System.out.println(areaRec1);
+
+        rectangle1.stampTwoRectangles(rectangle1, rectangle2);
+
+
+    }
+
+
+}
---
+++ src/es1/entities/Rectangle.java
@@ -0,0 +1,55 @@
+package es1.entities;
+
+public class Rectangle {
+
+    private int height;
+    private int width;
+
+
+    public Rectangle(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+
+    public int rectanglePerimeter() {
+        return (this.height + this.height) * 2;
+    }
+
+    public int rectangleArea() {
+        return this.height * this.width;
+    }
+
+    public void stampRectangle() {
+        System.out.println("The rectangle's perimeter is: " + rectanglePerimeter() + ". The area is: " + rectangleArea());
+    }
+
+    public void stampTwoRectangles(Rectangle rectangle1, Rectangle rectangle2) {
+        int perimeter1 = rectangle1.rectanglePerimeter();
+        int area1 = rectangle1.rectangleArea();
+
+        int perimeter2 = rectangle2.rectanglePerimeter();
+        int area2 = rectangle2.rectangleArea();
+
+        System.out.println("Rectangle 1 - Perimeter: " + perimeter1 + ", Area: " + area1);
+        System.out.println("Rectangle 2 - Perimeter: " + perimeter2 + ", Area: " + area2);
+        System.out.println("The sum of both perimeters is: " + (perimeter1 + perimeter2) + ", the sum of both areas is: " + (area1 + area2));
+
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+}
